2deadbb4a1666acf98f505390f3ba8b842c2720d
ericgold <egold05@gmail.com>
2017-03-27 19:59:53 -0700

    change button type from submit to button

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // trello api key 
 // 720ffeb378d4ec592d4c2ffa9b8f9dc7
 
-var submitButton = $('#submit-button');
+var cardButton = $('#card-button');
 
 // authentication
 
@@ -26,11 +26,12 @@ Trello.authorize({
 
 
 function makeCard() {
-	console.log('making a new card');
-	var myList = "58956480a99fc2e3a97efe3d";
+	var myList = '58956480a99fc2e3a97efe3d';
 	var creationSuccess = function(data) {
   		console.log('Card created successfully. Data returned:' + JSON.stringify(data));
 	};
+  
+  
 	var newCard = {
   		name: 'New Test Card', 
   		desc: 'This is the description of our new card.',
@@ -39,11 +40,11 @@ function makeCard() {
   		pos: 'top'
 	};
 	Trello.post('/cards/', newCard, creationSuccess);
-}
-
-
+};
 
 submitButton.click(makeCard);
+
+
 
 
 
